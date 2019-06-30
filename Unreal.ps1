5ad9d8a2d2e5a89c05bffb51d373fc315f7cb74e
darylglynn <50626364+darylglynn@users.noreply.github.com>
2019-06-30 19:26:33 +0100

    Update

--- Unreal.ps1
+++ Unreal.ps1
@@ -10,15 +10,15 @@
     [string]$MovieFormat = $null
 )
 
-Write-Output $outputName >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $ProjectLocation >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $MapName >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $LevelSequence >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $workingDirectory >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $ResX >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $ResY >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $MovieFrameRate >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
-Write-Output $MovieFormat >> "C:\Program Files\Epic Games\UE_4.22\Samples\Log.txt"
+Write-Output $outputName >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $ProjectLocation >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $MapName >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $LevelSequence >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $workingDirectory >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $ResX >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $ResY >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $MovieFrameRate >> "C:\Users\Public\Documents\batch.txt"
+Write-Output $MovieFormat >> "C:\Users\Public\Documents\batch.txt"
 
 
 cd "C:\Program Files\Epic Games\UE_4.22\Engine\Binaries\Win64\UE4Editor.exe"
